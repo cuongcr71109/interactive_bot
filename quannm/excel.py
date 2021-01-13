6c1f167ab00e49227be3906932ea9fc9eadcdf95
NMQ <ngminhquan.aries@gmail.com>
2021-01-13 15:19:44 +0700

    add data to database

--- quannm/excel.py
+++ quannm/excel.py
@@ -1,34 +1,47 @@
-from tkinter import Event
 import PySimpleGUI as sg
 import pandas as pd
 import mysql.connector
 
-account = []
-content = []
-order = []
 
 def getExcel():
+    # get file path in gui windows
     import_file_path = values['file']
+
+    # read data from excel file
     data = pd.read_excel(import_file_path)
+
+    # convert data to dataframe
     df = pd.DataFrame(data=data)
 
-    # global account, content, order
+    # Result:
+    #        Account Content  Order
+    # 0     Quan     abc      1
+    # 1    Cuong     Xyz      2
+    # 2     Phuc     Dce      3
+    # 3      Duc     ghf      1
+    # 4    Khang     abc      2
 
+    # get data along columns Account, Content, Order
     account = data.iloc[:, 0]
     content = data.iloc[:, 1]
     order = data.iloc[:, 2]
 
-    print(df)
+    # value to insert to DB
+    val = []
+    for index in df.index:
+        val.append((account[index], content[index], str(order[index])))
 
+    # Result:
+    # [('Quan', 'abc', 1), ('Cuong', 'Xyz', 2), ('Phuc', 'Dce', 3), ('Duc', 'ghf', 1), ('Khang', 'abc', 2)]
 
-    # ret
+    return val
     
 
 
 def upload():
-    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="test")
+    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="user")
     mycursor = mydb.cursor()
-    sql = "INSERT INTO user (Account, Content, Order) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO user (Account, Content, Ordered) VALUES (%s, %s, %s)"
     data = getExcel()
     mycursor.executemany(sql, data)
     mydb.commit()
@@ -38,6 +51,8 @@ window_title = 'Insert data'
 window_loc = (0,0)
 window_size = (800, 600)
 
+
+# GUI Window Layout
 layout = [
     [sg.Text('Choose file .xlsx to import:')],
     [sg.Input('Choose file .xlsx to import:', key='file'),
@@ -53,7 +68,7 @@ while True:
     
     if event == 'OK':
         getExcel()
-        # upload()
+        upload()
         break
 
 
